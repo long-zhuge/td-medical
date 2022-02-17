339586ba7f1c211e32908da9c8cc1b45e13168a1
long-zhuge <158362530@qq.com>
2022-02-17 14:44:41 +0800

    fix: MedicalEditor增加拖拽排序功能

---
+++ src/medical-editor/Middle/DragableItem.js
@@ -0,0 +1,92 @@
+import React, { useContext, useRef } from 'react';
+import FormOutlined from '@ant-design/icons/FormOutlined';
+import DeleteOutlined from '@ant-design/icons/DeleteOutlined';
+import { useDrag, useDrop } from 'react-dnd';
+import update from 'immutability-helper';
+import { Space, Popconfirm } from 'antd';
+import { LinkBtn } from 'td-antd';
+import Field from './Field';
+import './index.less';
+
+import { EditorContext } from '../index';
+
+const type = 'DragableMedicalItem';
+
+const DragableItem = ({ data, index }) => {
+  const { selectedElementList, setSelectedElementList, setFormData } = useContext(EditorContext);
+  const ref = useRef();
+
+  // 设置字段进行编辑
+  const onForm = (ele) => {
+    ele.index = index;
+    setFormData(ele);
+  };
+
+  // 删除组件
+  const onDelete = () => {
+    setFormData({});
+    setSelectedElementList(selectedElementList.reduce((p, c, cIndex) => index === cIndex ? p : [...p, c], []));
+  };
+
+  const [, drop] = useDrop({
+    accept: type,
+    collect: monitor => {
+      const { index: dragIndex } = monitor.getItem() || {};
+      if (dragIndex === index) {
+        return {};
+      }
+      return {
+        isOver: monitor.isOver(),
+      };
+    },
+    drop: item => {
+      setFormData({}); // 拖拽时，取消右侧编辑组件，否则会导致编辑错乱的问题
+      const dragRow = selectedElementList[item.index];
+      setSelectedElementList(update(selectedElementList, {
+        $splice: [
+          [item.index, 1],
+          [index, 0, dragRow],
+        ],
+      }));
+    },
+  });
+
+  const [, drag] = useDrag({
+    item: { type, index },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drop(drag(ref));
+
+  return (
+    <div className="td-medical-editor-middle-item">
+      <div ref={ref} className="middle-item-header">
+        <Space>
+          {data.cnName}
+          <LinkBtn onClick={() => onForm(data, index)}><FormOutlined /></LinkBtn>
+          <Popconfirm
+            title="确实删除该组件？"
+            onConfirm={() => onDelete(index)}
+          >
+            <LinkBtn danger><DeleteOutlined /></LinkBtn>
+          </Popconfirm>
+        </Space>
+      </div>
+      <div className="middle-item-content">
+        {data.fieldList.map((item, index2) => (
+          <Field
+            data={item}
+            index={index}
+            index2={index2}
+            method={data.method}
+            key={`${item.fieldNo}_${index}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DragableItem;
