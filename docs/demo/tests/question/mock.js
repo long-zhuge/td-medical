add5e8212ddf15fdeeae915d43ba08b0959162ae
long-zhuge <158362530@qq.com>
2022-06-08 13:14:36 +0800

    update question mock data

--- docs/demo/tests/question/mock.js
+++ docs/demo/tests/question/mock.js
@@ -1,121 +1,202 @@
-export default [
-  {
-    "questionBankNo": "Q00001",
-    "questionName": "你是男是女",
-    "questionNo": "101",
-    "questionType": "radio",
-    "optionList": [
-      {
-        "optionName": "男",
-        "optionNo": "100001",
-        "optionType": "1",
-        "questionNo": "101",
-        "skipQuestionNo": "102",
-      },
-      {
-        "optionName": "女",
-        "optionNo": "100002",
-        "optionType": "input",
-        "questionNo": "101",
-        "skipQuestionNo": "103",
-        "remark": '{"placeholder":"请输入"}',
-      },
-    ],
-  },
-  {
-    "questionBankNo": "Q00001",
-    "questionName": "你对中国移动的评分",
-    "questionNo": "102",
-    "questionType": "score",
-    "optionList": [
-      {
-        "optionName": "0分",
-        "optionNo": "100003",
-        "optionType": "1",
-        "questionNo": "102",
-        "skipQuestionNo": "104",
-        "remark": '没有帮助',
-      },
-      {
-        "optionName": "1分",
-        "optionNo": "100004",
-        "optionType": "1",
-        "questionNo": "102",
-        "skipQuestionNo": "104",
-      },
-      {
-        "optionName": "2分",
-        "optionNo": "100005",
-        "optionType": "1",
-        "questionNo": "102",
-        "skipQuestionNo": "104",
-      },
-      {
-        "optionName": "3分",
-        "optionNo": "100006",
-        "optionType": "1",
-        "questionNo": "102",
-        "skipQuestionNo": "104",
-        "remark": '非常有帮助',
-      },
-    ],
-  },
-  {
-    "questionBankNo": "Q00001",
-    "questionName": "本技术对您决策临床治疗方案的主要帮助是？（多选）",
-    "questionNo": "103",
-    "questionType": "checkbox",
-    "optionList": [
-      {
-        "optionName": "更加确定了原有的治疗方案",
-        "optionNo": "100007",
-        "optionType": "1",
-        "questionNo": "103",
-        "skipQuestionNo": "104",
-      },
-      {
-        "optionName": "提供了新的治疗思路",
-        "optionNo": "100008",
-        "optionType": "1",
-        "questionNo": "103",
-        "skipQuestionNo": "104",
-      },
-      {
-        "optionName": "其他（请说明）",
-        "optionNo": "100009",
-        "optionType": "input",
-        "questionNo": "103",
-        "skipQuestionNo": "104",
-      },
-    ],
-  },
-  {
-    "questionBankNo": "Q00001",
-    "questionName": "产品名称",
-    "questionNo": "104",
-    "questionType": "form",
-    "optionList": [
-      {
-        "optionName": "",
-        "optionNo": "100010",
-        "optionType": "input",
-        "questionNo": "104",
-        "skipQuestionNo": "105",
-      },
-    ],
-  },
-  {
-    "questionBankNo": "Q00001",
-    "questionName": "工作时间",
-    "questionNo": "105",
-    "questionType": "form",
-    "optionList": [
-      {
-        "optionName": "",
-        "optionNo": "100012",
-        "optionType": "rangeDate",
-        "questionNo": "105",
-      },
-    ],
-  },
-];
+export default [{
+  "createTime": "2022-06-01 12:05:41",
+  "id": "247",
+  "optionList": [{
+    "createTime": "2022-06-01 12:54:14",
+    "id": "1737",
+    "optionName": "",
+    "optionNo": "2022060150397761",
+    "optionType": "number",
+    "questionNo": "2022060149202368",
+    "skipQuestionNo": "2022060149329200"
+  }],
+  "productAnalysis": "other",
+  "questionName": "产品价格（单位：元）",
+  "questionNo": "2022060149202368",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-01 12:54:14"
+}, {
+  "createTime": "2022-06-01 12:06:03",
+  "id": "248",
+  "optionList": [{
+    "createTime": "2022-06-01 12:09:14",
+    "id": "1669",
+    "optionNo": "2022060150465165",
+    "optionType": "number",
+    "questionNo": "2022060149329200",
+    "skipQuestionNo": "2022060149834233"
+  }],
+  "productAnalysis": "other",
+  "questionName": "年度配送规模（单位：万）",
+  "questionNo": "2022060149329200",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-01 12:09:14"
+}, {
+  "createTime": "2022-06-01 12:06:17",
+  "id": "249",
+  "optionList": [{
+    "createTime": "2022-06-01 12:09:18",
+    "id": "1670",
+    "optionNo": "2022060150134166",
+    "optionType": "rangeDate",
+    "questionNo": "2022060149834233",
+    "skipQuestionNo": "2022060149658071"
+  }],
+  "productAnalysis": "other",
+  "questionName": "执行日期",
+  "questionNo": "2022060149834233",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-01 12:09:18"
+}, {
+  "createTime": "2022-06-01 12:06:41",
+  "id": "250",
+  "optionList": [{
+    "createTime": "2022-06-01 12:09:23",
+    "id": "1671",
+    "optionNo": "2022060150580071",
+    "optionType": "number",
+    "questionNo": "2022060149658071",
+    "skipQuestionNo": "2022060149616758"
+  }],
+  "productAnalysis": "other",
+  "questionName": "回款周期（单位：天）",
+  "questionNo": "2022060149658071",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-01 12:09:23"
+}, {
+  "createTime": "2022-06-01 12:06:56",
+  "id": "251",
+  "optionList": [{
+    "createTime": "2022-06-01 12:09:32",
+    "id": "1672",
+    "optionNo": "2022060150570205",
+    "optionType": "number",
+    "questionNo": "2022060149616758",
+    "skipQuestionNo": "2022060149220589"
+  }],
+  "productAnalysis": "other",
+  "questionName": "覆盖医院数量（单位：家）",
+  "questionNo": "2022060149616758",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-01 12:09:32"
+}, {
+  "createTime": "2022-06-01 12:07:20",
+  "id": "252",
+  "optionList": [{
+    "createTime": "2022-06-01 12:09:36",
+    "id": "1673",
+    "optionName": "负责人名称",
+    "optionNo": "2022060150090126",
+    "optionType": "input",
+    "questionNo": "2022060149220589",
+    "skipQuestionNo": "2022060149414629"
+  }, {
+    "createTime": "2022-06-01 12:09:36",
+    "id": "1674",
+    "optionName": "手机号",
+    "optionNo": "2022060150320396",
+    "optionType": "input",
+    "questionNo": "2022060149220589",
+    "skipQuestionNo": "2022060149414629"
+  }],
+  "productAnalysis": "other",
+  "questionName": "负责人及电话",
+  "questionNo": "2022060149220589",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-01 12:09:36"
+}, {
+  "createTime": "2022-06-01 12:07:44",
+  "id": "253",
+  "optionList": [{
+    "createTime": "2022-06-08 10:21:25",
+    "id": "1802",
+    "optionNo": "2022060850908411",
+    "optionType": "number",
+    "questionNo": "2022060149414629",
+    "skipQuestionNo": "2022060149336761"
+  }],
+  "productAnalysis": "other",
+  "questionName": "配送费率（%）",
+  "questionNo": "2022060149414629",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-08 10:21:25"
+}, {
+  "createTime": "2022-06-01 12:07:59",
+  "id": "254",
+  "optionList": [{
+    "createTime": "2022-06-08 10:23:10",
+    "id": "1808",
+    "optionNo": "2022060850668802",
+    "optionType": "input",
+    "questionNo": "2022060149336761",
+    "skipQuestionNo": "2022060149260461"
+  }],
+  "productAnalysis": "other",
+  "questionName": "目标市场",
+  "questionNo": "2022060149336761",
+  "questionType": "form",
+  "type": "customize",
+  "updateTime": "2022-06-08 10:23:10"
+}, {
+  "createTime": "2022-06-01 12:08:41",
+  "id": "255",
+  "optionList": [{
+    "createTime": "2022-06-08 10:22:29",
+    "id": "1804",
+    "optionName": "合作机会很大",
+    "optionNo": "2022060850676100",
+    "questionNo": "2022060149260461",
+    "skipQuestionNo": "2022041249790153"
+  }, {
+    "createTime": "2022-06-08 10:22:29",
+    "id": "1805",
+    "optionName": "合作机会中等",
+    "optionNo": "2022060850883572",
+    "questionNo": "2022060149260461",
+    "skipQuestionNo": "2022041249790153"
+  }, {
+    "createTime": "2022-06-08 10:22:29",
+    "id": "1806",
+    "optionName": "合作机会很小",
+    "optionNo": "2022060850863480",
+    "questionNo": "2022060149260461",
+    "skipQuestionNo": "2022041249790153"
+  }, {
+    "createTime": "2022-06-08 10:22:29",
+    "id": "1807",
+    "optionName": "无合作机会",
+    "optionNo": "2022060850683843",
+    "questionNo": "2022060149260461",
+    "skipQuestionNo": "2022041249790153"
+  }],
+  "productAnalysis": "other",
+  "questionName": "合作机会",
+  "questionNo": "2022060149260461",
+  "questionType": "radio",
+  "type": "customize",
+  "updateTime": "2022-06-08 10:22:29"
+}, {
+  "createTime": "2022-04-12 11:21:48",
+  "id": "138",
+  "optionList": [{
+    "createTime": "2022-04-12 11:21:48",
+    "id": "917",
+    "optionNo": "2022041250048603",
+    "optionType": "region",
+    "questionNo": "2022041249790153",
+    "remark": "{\"placeholder\":\"请选择省市区\"}"
+  }],
+  "productAnalysis": "other",
+  "questionName": "地址",
+  "questionNo": "2022041249790153",
+  "questionType": "form",
+  "type": "standard",
+  "updateTime": "2022-06-08 10:22:20"
+}];
